e8682c69dccbe01d7337025c09fae535c42e66ff
João Caio Lopes Bernardino <jocaio@uol.com.br>
2021-10-25 11:33:27 -0300

    Aula array b7w

---
+++ script.js
@@ -0,0 +1,13 @@
+let carros = [
+    'Palio',
+    'Uno',
+    'Corola',
+    'Ferrari'
+];
+
+let ingredientes = [
+    ['uva', 'pera', 'maça'],
+    ['arroz', 'macarrao']
+];
+
+console.log(ingredientes[1][0])
